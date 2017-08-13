502daa9e0a71ad24da102b78bb44bd8f353cd6c8
susanev <susan.ra.evans@gmail.com>
2017-08-12 18:27:50 -0700

    added face without functions

---
+++ 1-day-workshop/activities/face-sample-projects/basic-face/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(300, 300);
+}
+
+function draw() {
+  noFill();
+  ellipse(100, 100, 100, 100);
+  ellipse(125, 100, 20, 20);
+  ellipse(200, 100, 100, 100);
+  ellipse(175, 100, 20, 20);
+  line(125, 200, 175, 200);
+}
